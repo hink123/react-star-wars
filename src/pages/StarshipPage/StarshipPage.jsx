cc6e76fdfae3ad68610c761afd6650de2b22b996
Will Hinkelman <will.hink24@gmail.com>
2020-01-14 20:59:36 -0800

    detail page styling

--- src/pages/StarshipPage/StarshipPage.jsx
+++ src/pages/StarshipPage/StarshipPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './StarshipPage.css';
 
 function StarshipPage({location}) {
     const starship = location.state;
     return (
-        <div>
+        <div className='starship-container'>
             <h3>Name: {starship.name}</h3>
             <h3>Model: {starship.model}</h3>
-            <Link to="/">Return</Link>
+            <Link to="/" className="link">Return</Link>
         </div>
     )
 }
